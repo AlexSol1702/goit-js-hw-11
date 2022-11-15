6ffa092ee4e639750be786ab942ebdc6333db11e
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-11-15 12:07:14 +0200

    update

---
+++ src/api.js
@@ -0,0 +1,36 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '31251439-64cf22bfdeb9633faeca9a5f6';
+
+
+
+export default class NewApi {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    
+   
+    async fetchArticles(){
+
+        const url = `${BASE_URL}?key=${KEY}&q=${this.searchQuery}}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`    
+
+        const response = await fetch(url)
+        const data = await response.json()
+        this.page += 1
+        return data
+                
+             
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+   
+}
---
+++ src/index.js
@@ -0,0 +1,98 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { axios } from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import NewApi from "./api";
+
+const getEl = selector => document.querySelector(selector)
+
+getEl('.search-form').addEventListener('submit', onSubmit)
+getEl('.load-more').addEventListener('click', onMore)
+
+let cardMarkup = ''
+const newApi = new NewApi()
+const modalWindow = new SimpleLightbox('.gallery a');
+modalWindow.on('show.simplelightbox', function () {});
+
+
+function onSubmit  (e) {
+        e.preventDefault()
+
+        newApi.resetPage()
+        newApi.query = getEl('input').value
+        if (getEl('input').value === ''|| getEl('input').value === " ") {
+          return 
+        }
+
+        newApi.fetchArticles()
+          .then(data =>{ if (data.hits.length === 0) {
+            throw new Error() 
+          } else {
+             
+              Notify.success(`Hooray! We found ${data.totalHits} images.`)
+              getEl('.gallery').innerHTML = ''
+              drawMarkup(data.hits) 
+              addMarkup(cardMarkup)  
+              getEl('.load-more').classList.add('active')                    
+          }})
+          .catch(onError)         
+          
+               
+    }
+
+function onMore() {
+        newApi.fetchArticles()
+          .then(data => 
+            drawMarkup(data.hits),
+            addMarkup(cardMarkup),
+                        
+           )  
+    
+            
+}    
+
+function onError(err) {   
+           console.log(err)  
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+       
+function drawMarkup(arr) {
+        cardMarkup = arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+        return `<div class="photo-card">
+        <a class="gallery__item" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            <b>${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <b>${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <b>${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            <b>${downloads}</b>
+          </p>
+        </div>
+      </div>`
+        }).join('')
+      
+        
+      //   if(data.totalHits < page * 40) {
+      //     Notify.failure("We're sorry, but you've reached the end of search results.")
+      //     getEl('load-more').classList.remove('active')
+      // } 
+      
+        
+}    
+
+function addMarkup(markup) {
+    getEl('.gallery').insertAdjacentHTML('beforeend', markup)
+}
+
+
+
